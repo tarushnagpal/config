0f775dcfec39c43f5eb25897d001e39aa6bcde99
tarushnagpal <tarush.nagpal7@gmail.com>
2026-03-12 17:07:37 +0530

    copilot package

--- doom/packages.el
+++ doom/packages.el
@@ -33,6 +33,8 @@
   ;; Corrects (and improves) org-mode's native fontification.
   (doom-themes-org-config))
 
+(package! copilot
+  :recipe (:host github :repo "copilot-emacs/copilot.el" :files ("*.el")))
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/radian-software/straight.el#the-recipe-format
